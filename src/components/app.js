a5135e4fed5f3a976f5ae883b57356485aa8b330
casonhawley <cason.hawley@gmail.com>
2018-11-28 10:00:45 -0700

    added pages as well as content

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 import Welcome from './welcome';
+import Navigation from './navigation';
+import About from './about';
 
 class App extends Component {
   render() {
     return (
       <div className='app'>
+        <Navigation />
         <Welcome />
+        <About />
       </div>
     );
   }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,11 @@ const Navigation = () => {
     return (
       <div className='navigation'>
         <div>Logo</div>
+        <div>Link 1</div>
+        <div>Link 2</div>
+        <div>Link 3</div>
+        <div>Link 4</div>
+        <div>Link 5</div>
       </div>
     );
   }
